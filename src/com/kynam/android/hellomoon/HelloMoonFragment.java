03a5ad19c9a8f518195668cc2979f58b27e290ea
zhilun <trankynam2612@gmail.com>
2014-08-06 15:41:57 +0700

    Trang 235

--- src/com/kynam/android/hellomoon/HelloMoonFragment.java
+++ src/com/kynam/android/hellomoon/HelloMoonFragment.java
@@ -8,6 +8,8 @@ import android.view.ViewGroup;
 import android.widget.Button;
 
 public class HelloMoonFragment extends Fragment {
+	private AudioPlayer mPlayer = new AudioPlayer();
+	
 	private Button mPlayButton;
 	private Button mStopButton;
 	
@@ -17,9 +19,26 @@ public class HelloMoonFragment extends Fragment {
 		View v = inflater.inflate(R.layout.fragment_hello_moon, parent, false);
 		
 		mPlayButton = (Button)v.findViewById(R.id.hellomoon_playButton);
+		mPlayButton.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				mPlayer.play(getActivity());
+			}
+		});
+		
 		mStopButton = (Button)v.findViewById(R.id.hellomoon_stopButton);
+		mStopButton.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				mPlayer.stop();
+			}
+		});
 		
 		return v;
+	}
+	
+	@Override
+	public void onDestroy() {
+		super.onDestroy();
+		mPlayer.stop();
 	}
 
 }
